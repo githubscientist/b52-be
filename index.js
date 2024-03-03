2cef6a95977b0eb5e5adb0bda976b2a1f5dfa489
githubscientist <sathish.datascientist@gmail.com>
2024-03-03 15:15:38 +0530

    local db nodejs mongoose connection

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello world!');
+// import mongoose
+const mongoose = require('mongoose');
+
+// connect to the database
+console.log('Connecting to the database...');
+mongoose.connect('mongodb://localhost:27017/b52db')
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch((err) => {
+        console.log('Error connecting to the database', err);
+    });
