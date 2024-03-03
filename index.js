f066072723d9925e6ee8f7a46dee24b5f67dfe4f
githubscientist <sathish.datascientist@gmail.com>
2024-03-03 15:28:51 +0530

    started express server

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+// import express
+const express = require('express');
+
+// create a server
+const app = express();
+
 // import mongoose
 const mongoose = require('mongoose');
 
-// connect to the database
+// connect to the database locally
 console.log('Connecting to the database...');
-mongoose.connect('mongodb://localhost:27017/b52db')
+// mongoose.connect('mongodb://localhost:27017/b52db')
+//     .then(() => {
+//         console.log('Connected to the database');
+//     })
+//     .catch((err) => {
+//         console.log('Error connecting to the database', err);
+//     });
+
+// connect to the database on the cloud
+mongoose.connect('mongodb+srv://sathishdatascientist:Guvi2023@cluster0.2r9mjwk.mongodb.net/')
     .then(() => {
         console.log('Connected to the database');
+        // start the server and listen to the port 3001
+        app.listen(3001, () => {
+            console.log(`Server running at http://localhost:3001/`);
+        });
     })
     .catch((err) => {
         console.log('Error connecting to the database', err);
